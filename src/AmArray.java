253c9bc8b1340b6324d50938db9cb6dd74d6601b
jo4odapadaria <joao_pedro_tonetti@hotmail.com>
2025-04-05 17:23:39 -0300

    arraiá

---
+++ src/AmArray.java
@@ -0,0 +1,13 @@
+public class AmArray {
+
+    public static void main(String[] args){
+
+        String alunos [] = {"0Moises","1Tiago","2Camilo","3Mohammed"} ;
+
+        for ( String Aluno : alunos ) {
+
+            System.out.println(" Quem vai explodir é: " + Aluno );
+        }
+        
+    }
+}
---
+++ src/SheepCount.java
@@ -0,0 +1,9 @@
+public class SheepCount {
+
+    public static void main(String[] args) {
+
+        for( int sheep = 1; sheep <= 10; sheep++) {
+            System.out.println( " Hum..." + sheep + " ovelhinha" );
+        }
+    }
+}
